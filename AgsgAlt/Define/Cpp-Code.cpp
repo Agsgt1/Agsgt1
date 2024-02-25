da62eaf324420afa1d5615b561724ea583185f56
Agsgt1 <138738952+Agsgt1@users.noreply.github.com>
2024-02-25 15:51:57 -0600

    Create Cpp-Code.cpp

---
+++ AgsgAlt/Define/Cpp-Code.cpp
@@ -0,0 +1,31 @@
+#define int makeclass //used for making classes
+#define = is
+#define == reallyis
+#define ; RIGHTNOW
+#define if istwo
+#define false no //DISCLAIMER: not a threat
+#define -- hitlerized
+#define ++ isgod
+#define } ded
+#define else if BULLSHIT //ArnoldC refrence?
+#define println makern
+#define else dedtoo
+#define >= iles
+#define <= trues
+#define break fuck
+
+makeclass main() {
+  i is 
+  istwo (i iles 7) {
+    istwo (i trues 6) { 
+      i hitlerized
+    ded dedtoo { 
+      fuck
+    ded
+  ded BULLSHIT (i reallyis 1) {
+    makern(i) RIGHTNOW
+  ded dedtoo {
+    i isgod
+  ded
+  i is no
+ded
