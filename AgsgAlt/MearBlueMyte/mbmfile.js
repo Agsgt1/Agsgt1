aaa4e13c389cb6039456897f5b42db59ebef09d3
Agsgt1 <138738952+Agsgt1@users.noreply.github.com>
2024-02-25 15:30:01 -0600

    Create mbmfile.js

---
+++ AgsgAlt/MearBlueMyte/mbmfile.js
@@ -0,0 +1,18 @@
+import { Observer, Signal } from "./habitat.js"
+function letdudebe(x) {
+  let dude = x
+}
+
+function giveChild(x) {
+  let Child = x
+}
+
+function getChildSearchHistory() {
+  return Child
+}
+
+function cmeredude() {
+  return dude
+}
+
+//todepond make code here bcz i wuz here ._.
